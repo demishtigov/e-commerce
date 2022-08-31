a7919367eb62ded06129fe3c0efa5984eb9fd788
Magomed <93042393+demishtigov@users.noreply.github.com>
2022-08-31 11:16:19 +0300

    added sanity schemas

--- e-commerce_server/schemas/schema.js
+++ e-commerce_server/schemas/schema.js
@@ -1,3 +1,5 @@
+import product from './Product'
+import banner from './Banner'
 // First, we must import the schema creator
 import createSchema from 'part:@sanity/base/schema-creator'
 
@@ -11,6 +13,6 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    product, banner
   ]),
 })
